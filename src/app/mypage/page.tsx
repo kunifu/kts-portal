30994116ff913ae943f0ff1b5b73a6846ca0db7d
TAKI <shimoty.k@gmail.com>
2023-05-06 19:59:28 +0900

    マイページ作成

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,28 @@
-const App = () => {
-  return <div>hoge</div>;
-};
+"use client";
 
-export default App;
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+
+import awsExports from "../../aws-exports";
+Amplify.configure(awsExports);
+
+export default function App() {
+  return (
+    <Authenticator signUpAttributes={["nickname"]}>
+      <MyPage />
+    </Authenticator>
+  );
+}
+
+function MyPage() {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+  return (
+    <main>
+      <h1>{user?.attributes?.nickname}さん KTS Portalへようこそ</h1>
+      <h2>{user?.attributes?.email} でログインしています。</h2>
+      <button onClick={signOut}>Sign out</button>
+    </main>
+  );
+}
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export enum Path {
   Index = "/",
-  Dashboard = "dashboard",
+  MyPage = "mypage",
 }
