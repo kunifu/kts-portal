9b9be35b1352e43a8aa87d68bb758ec8197568f6
Daiki Ko <dk.kunifu@gmail.com>
2023-05-07 17:54:40 +0900

    設定値を環境変数から読み出すように変更

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -5,8 +5,32 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 
-import awsExports from "../../aws-exports";
-Amplify.configure(awsExports);
+Amplify.configure({
+  "aws_project_region": process.env.PROJECT_REGION,
+  "aws_cognito_identity_pool_id": process.env.COGNITO_IDENTITY_POOL_ID,
+  "aws_cognito_region": process.env.COGNITO_REGION,
+  "aws_user_pools_id": process.env.USER_POOLS_ID,
+  "aws_user_pools_web_client_id": process.env.USER_POOLS_WEB_CLIENT_ID,
+  "oauth": {},
+  "aws_cognito_username_attributes": [
+      "EMAIL"
+  ],
+  "aws_cognito_social_providers": [],
+  "aws_cognito_signup_attributes": [
+      "EMAIL"
+  ],
+  "aws_cognito_mfa_configuration": "OFF",
+  "aws_cognito_mfa_types": [
+      "SMS"
+  ],
+  "aws_cognito_password_protection_settings": {
+      "passwordPolicyMinLength": 8,
+      "passwordPolicyCharacters": []
+  },
+  "aws_cognito_verification_mechanisms": [
+      "EMAIL"
+  ]
+});
 
 export default function App() {
   return (
