50c1f5d144bedd400d90a898bc85dac32c6fa597
Daiki Ko <dk.kunifu@gmail.com>
2023-05-09 00:00:48 +0900

    サーバサイドで環境変数を扱えるように修正 fyi https://zenn.dev/thim/articles/04775c68d796445f3c90

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
       },
     ];
   },
+  env: {
+    PROJECT_REGION: process.env.PROJECT_REGION,
+    COGNITO_IDENTITY_POOL_ID: process.env.COGNITO_IDENTITY_POOL_ID,
+    COGNITO_REGION: process.env.COGNITO_REGION,
+    USER_POOLS_ID: process.env.USER_POOLS_ID,
+    USER_POOLS_WEB_CLIENT_ID: process.env.USER_POOLS_WEB_CLIENT_ID,
+  }
 };
 
 module.exports = nextConfig;
